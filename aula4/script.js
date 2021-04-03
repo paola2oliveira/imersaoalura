14e094aa495089af96bfd636ab993a784008f01b
Paola Oliveira <80711521+paola2oliveira@users.noreply.github.com>
2021-04-03 13:42:10 -0300

    Add files via upload

---
+++ aula4/script.js
@@ -0,0 +1,5 @@
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BOTk5ODg0OTU5M15BMl5BanBnXkFtZTgwMDQ3MDY3NjM@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + ">")
+}
